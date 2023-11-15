ea1b44695b40e16d570e5487941b9e05dee8626c
Dominic <dorlando@ucsd.edu>
2023-11-15 13:24:17 -0800

    Well need to follow the actual instructions for the assignment but for now not bad

--- js/message-add.js
+++ js/message-add.js
@@ -1,23 +1,48 @@
-document.addEventListener("DOMContentLoaded", (event)=>{
-    let form = document.getElementsByTagName("form");
-    console.log(form);
+document.addEventListener("DOMContentLoaded", (event) => {
+    let form = document.getElementsByTagName('form')[0];
+    let name = form.querySelector('#contact-form-name');
+    let email = form.querySelector('#contact-form-email');
+    let message = form.querySelector('#contact-form-comments');
+    let message_holder = document.querySelector('#message-wrapper');
+    get_existing_messages(700);
+    //Maybe not the best thing to be doing, but this at least makes it so the images are different between each other.
+
     document.getElementById('submit').addEventListener('click', (event) => {
         event.preventDefault();
         let time = new Date();
-        let name = document.getElementById('contact-form-name').value;
-        let email = document.getElementById('contact-form-email').value;
-        let message = document.getElementById('contact-form-message').value;
-        let clone = document.querySelector('template').content.cloneNode(true);
-        let messages = document.getElementById('message-wrapper');
-        let image = clone.querySelector('img');
-        clone.querySelector('.message').setAttribute('data-email', email); 
-        image.setAttribute('alt', `Picture of ${name}`);
-        console.log(clone);
-        console.log(name);
-        clone.querySelector('.message-sender').innerHTML = `${name} <time datetime="${time.toString()}">${time.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true})}</time>`
-        clone.querySelector('.message-content').innerHTML = message;
-        console.log(message);
-        console.log(clone);
-        messages.append(clone);
+        let date_time = time.toString();
+        let time_print = time.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
+        append_message(name.value, email.value, message.value, date_time, time_print);
+        let stored_messages = JSON.parse(localStorage.getItem('messages') || null); 
+        let new_message = {'name': name.value, 'email': email.value, 'message': message.value, 'time': date_time, 'time_to_print': time_print};
+        if (stored_messages === null) {
+            stored_messages = [new_message];
+            console.log(stored_messages);
+        } else {
+            stored_messages = stored_messages.concat(new_message);
+        }
+        localStorage.setItem('messages', JSON.stringify(stored_messages));
+        form.reset();
     });
+
+    function append_message(name, email, comment, time, time_to_print) {
+        let clone = document.querySelector('template').content.cloneNode(true);
+        clone.querySelector('.message').setAttribute('data-email', email);
+        clone.querySelector('img').setAttribute('alt', `Picture of ${name}`);
+        clone.querySelector('.message-sender').innerHTML = `${name} <time datetime="${time.toString()}">${time_to_print}</time>`;
+        clone.querySelector('.message-content').innerHTML = comment;
+        message_holder.append(clone);
+    }
+
+    async function get_existing_messages(delay) {
+        let message_list = JSON.parse(localStorage.getItem('messages') || null);
+        if (message_list !== null) {
+            for(let i = 0; i < message_list.length; i++) {
+                let message = message_list[i];
+                append_message(message.name, message.email, message.message, message.time, message.time_to_print);
+                await new Promise(x => setTimeout(x, delay));
+            }
+        }
+    }
 });
+
