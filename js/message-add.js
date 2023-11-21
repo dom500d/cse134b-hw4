2ec46842cdcce9a9c1e4a778d3ddb3efd561a1f1
Dominic <dorlando@ucsd.edu>
2023-11-21 14:55:42 -0800

    more JS work done its not bad

--- js/message-add.js
+++ js/message-add.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         let time = new Date();
         let date_time = time.toString();
         let time_print = time.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
-        append_message(name.value, email.value, message.value, date_time, time_print);
+        let image_data = append_message(name.value, email.value, message.value, date_time, time_print);
         let stored_messages = JSON.parse(localStorage.getItem('messages') || null); 
-        let new_message = {'name': name.value, 'email': email.value, 'message': message.value, 'time': date_time, 'time_to_print': time_print};
+        let new_message = {'name': name.value, 'email': email.value, 'message': message.value, 'time': date_time, 'time_to_print': time_print, 'img': image_data};
         if (stored_messages === null) {
             stored_messages = [new_message];
             console.log(stored_messages);
@@ -22,16 +22,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
             stored_messages = stored_messages.concat(new_message);
         }
         localStorage.setItem('messages', JSON.stringify(stored_messages));
+        // get_image_bytes();
         form.reset();
+    });
+
+    document.getElementById('remove-messages').addEventListener('click', (event) => {
+        localStorage.removeItem('messages');
+        let nodes = document.querySelectorAll('[data-email]');
+        console.log(nodes);
+        for (let i = 0; i < nodes.length; i++) {
+            message_holder.removeChild(nodes[i]);
+        }
     });
 
     function append_message(name, email, comment, time, time_to_print) {
         let clone = document.querySelector('template').content.cloneNode(true);
+        let image = clone.querySelector('picture');
         clone.querySelector('.message').setAttribute('data-email', email);
         clone.querySelector('img').setAttribute('alt', `Picture of ${name}`);
         clone.querySelector('.message-sender').innerHTML = `${name} <time datetime="${time.toString()}">${time_to_print}</time>`;
         clone.querySelector('.message-content').innerHTML = comment;
         message_holder.append(clone);
+
+        return image;
     }
 
     async function get_existing_messages(delay) {
@@ -43,6 +56,27 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 await new Promise(x => setTimeout(x, delay));
             }
         }
+    }
+
+    async function get_image_bytes() {
+        await fetch('https://thispersondoesnotexist.com/', {
+            method: 'GET',
+            mode: 'no-cors'
+        }).then(response => {
+            console.log(response);
+        })
+    }
+
+    function get_image() {
+        console.log(img);
+        let image = img.querySelector('img');
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d');
+        canvas.height = image.naturalHeight;
+        canvas.width = image.naturalWeight;
+        context.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
+        console.log(canvas.toDataURL('image/png'));
+        return canvas.toDataURL();
     }
 });
 
