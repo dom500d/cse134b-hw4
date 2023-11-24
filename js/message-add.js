5b37312fc99fe290fee25a884a9e261cd8cc96e6
Dominic <dorlando@ucsd.edu>
2023-11-24 09:21:51 -0800

    Added color theme changed

--- js/message-add.js
+++ js/message-add.js
@@ -4,8 +4,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let email = form.querySelector('#contact-form-email');
     let message = form.querySelector('#contact-form-comments');
     let message_holder = document.querySelector('#message-wrapper');
+    let root = document.querySelector(':root');
+    let background_color = document.querySelector('#background-color');
+    let text_color = document.querySelector('#text-color');
+    let theme_toggle = document.querySelector('#color-theme-toggle');
+    let reset_theme_button = document.querySelector('#reset-theme');
+
+    let stored_theme = localStorage.getItem('theme') || null;
+    theme_apply(stored_theme);
+
     get_existing_messages(700);
     //Maybe not the best thing to be doing, but this at least makes it so the images are different between each other.
+
+    background_color.addEventListener('input', change_color);
+    text_color.addEventListener('input', change_color);
+    theme_toggle.addEventListener('input', change_theme);
+    reset_theme_button.addEventListener('click', reset_theme);
 
     document.getElementById('submit').addEventListener('click', (event) => {
         event.preventDefault();
@@ -77,6 +91,66 @@ document.addEventListener("DOMContentLoaded", (event) => {
         context.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
         console.log(canvas.toDataURL('image/png'));
         return canvas.toDataURL();
+    }
+
+    function change_color(event) {
+        if(event.target.id === 'text-color') {
+            if(theme_toggle.checked) {
+                root.style.setProperty('--left-text-color', text_color.value);
+                localStorage.setItem('dark--left-text-color', text_color.value);
+            } else {
+                root.style.setProperty('--left-text-color', text_color.value);
+                localStorage.setItem('--left-text-color', text_color.value);
+            }  
+        } else if(event.target.id === 'background-color') {
+            if(theme_toggle.checked) {
+                root.style.setProperty('--left-color', background_color.value);
+                localStorage.setItem('dark--left-color', background_color.value);
+            } else {
+                root.style.setProperty('--left-color', background_color.value);
+                localStorage.setItem('--left-color', background_color.value);
+            }
+        } else {
+            console.log('Something went wrong when changing the color theme');
+        }
+    }
+
+    function change_theme() {
+        let theme = theme_toggle.checked ? 'dark' : 'light'
+        localStorage.setItem('theme', theme);
+        theme_apply(theme);
+    }
+
+    function theme_apply(state) {
+        if(state === 'dark') {
+            theme_toggle.checked = true;
+                
+            root.style.setProperty('--left-color', localStorage.getItem('dark--left-color'));
+            root.style.setProperty('--left-text-color', localStorage.getItem('dark--left-text-color'));
+        } else if (state === 'light') {
+            theme_toggle.checked = false;
+            
+            root.style.setProperty('--left-color', localStorage.getItem('--left-color'));
+            root.style.setProperty('--left-text-color', localStorage.getItem('--left-text-color'));
+        }
+        apply_computed_style();
+       
+    }
+
+    function reset_theme() {
+        theme_toggle.checked = false;
+        root.style.setProperty('--left-color', '#a80ea1');
+        root.style.setProperty('--left-text-color', '#e0e0e0');
+        localStorage.removeItem('--left-color');
+        localStorage.removeItem('--left-text-color');
+        localStorage.removeItem('dark--left-color');
+        localStorage.removeItem('dark--left-text-color');
+        apply_computed_style();
+    }
+
+    function apply_computed_style() {
+        background_color.value = getComputedStyle(root).getPropertyValue('--left-color');
+        text_color.value = getComputedStyle(root).getPropertyValue('--left-text-color');
     }
 });
 
